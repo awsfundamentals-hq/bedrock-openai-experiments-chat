393d3191a7de39db9f095beb68a87bf3ca604097
Tobias Schmidt <schmidt.tobias@protonmail.ch>
2024-04-16 07:28:14 +0200

    add notes to context

--- packages/functions/src/openai-api.ts
+++ packages/functions/src/openai-api.ts
@@ -1,4 +1,5 @@
 import { DynamoDbAdapter } from '@bedrock-rag/core/adapter/database/dynamodb.adapter';
+import { ChatsEntity } from '@bedrock-rag/core/adapter/database/model/chats';
 import { OpenAiAdapter } from '@bedrock-rag/core/adapter/openai/openai.adapter';
 import { checkApiKey } from '@bedrock-rag/core/utils/core';
 import { ApiHandler } from 'sst/node/api';
@@ -18,9 +19,29 @@ export const listModels = ApiHandler(async (_evt) => {
 
 export const submit = ApiHandler(async (evt) => {
   checkApiKey(evt);
-  const { model, content: prompt } = JSON.parse(evt.body!);
-  const previousMessages = await dynamoDbAdapter.listMessages();
+  const parsedBody = JSON.parse(evt.body!);
+  const { model } = parsedBody;
+  let { content: prompt } = parsedBody;
+  const previousMessages: ChatsEntity[] = await dynamoDbAdapter.listMessages();
   await dynamoDbAdapter.createMessage({ content: prompt, role: 'user' });
+  // the oldest message always includes the notes
+  const notes = await dynamoDbAdapter.list();
+  console.info(`Adding ${notes.length} notes to the prompt`);
+  const joinedNotes =
+    `These are some relevant notes I have taken. ` +
+    `Please only consider using this information if it ` +
+    `seems useful for the questions that I ask you.` +
+    `DO NOT CONFIRM THAT you understood this. ` +
+    `Continue with our chat as if ` +
+    `I never provided any additional information ` +
+    `in the first place: \n${notes.map(({ description, text }) => `* ${description}: ${text}`).join('\n')}`;
+  if (previousMessages.length) {
+    const oldestMessage = previousMessages[0];
+    oldestMessage.content = `${joinedNotes}\n${oldestMessage.content}`;
+  } else {
+    prompt = `${joinedNotes}\n${prompt}`;
+  }
+  console.info(JSON.stringify(previousMessages, null, 2));
   const content = await openAiAdapter.submitPrompt(prompt, previousMessages, model);
   await dynamoDbAdapter.createMessage({ content: content, role: 'assistant' });
   return {
